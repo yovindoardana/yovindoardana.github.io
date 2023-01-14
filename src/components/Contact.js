9bdf4b0bc71dd79743e9593e6c20253dd7bd9ed9
yovindoardana <yovindoardanaa@gmail.com>
2023-01-15 02:09:04 +0700

    Try Netlify Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,7 @@
 import React from 'react'
+import { NetlifyForm, Honeypot } from 'react-netlify-forms'
 
-export default function Contact () {
-  const [name, setName] = React.useState('')
-  const [email, setEmail] = React.useState('')
-  const [message, setMessage] = React.useState('')
-
-  function encode (data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-      )
-      .join('&')
-  }
-
-  function handleSubmit (e) {
-    e.preventDefault()
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', name, email, message })
-    })
-      .then(() => alert('Message sent!'))
-      .catch((error) => alert(error))
-  }
-
+const Contact = () => {
   return (
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
@@ -63,13 +41,16 @@ export default function Contact () {
             </div>
           </div>
         </div>
-        <form
-          data-netlify="true"
-          hidden
-          name="contact"
-          onSubmit={handleSubmit}
-          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+        <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <NetlifyForm honeypotName='bot-field' name="Contact" action="/thanks">
+          { ({ handleChange, success, error }) => (
+            <>
+              <Honeypot />
+              { success && <p>Thanks for contacting me!</p> }
+              { error && (
+                <p>Sorry, we could not reach our servers. Please try again later.</p>
+              )}
+              <h2 className="text-white sm:text-4xl text-3xl mb-1 mt-10 sm:mt-0 font-medium title-font">
             Contact Me
           </h2>
           <div className="relative mb-4">
@@ -81,7 +62,7 @@ export default function Contact () {
               id="name"
               name="name"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setName(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <div className="relative mb-4">
@@ -93,7 +74,7 @@ export default function Contact () {
               id="email"
               name="email"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <div className="relative mb-4">
@@ -106,7 +87,7 @@ export default function Contact () {
               id="message"
               name="message"
               className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              onChange={(e) => setMessage(e.target.value)}
+              onChange={handleChange}
             />
           </div>
           <button
@@ -114,8 +95,13 @@ export default function Contact () {
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
-        </form>
+            </>
+          ) }
+        </NetlifyForm>
+        </div>
       </div>
     </section>
   )
 }
+
+export default Contact
