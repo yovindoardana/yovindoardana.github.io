611093dcd588a2ae79eb300db6261beabbc610b2
yovindoardana <yovindoardanaa@gmail.com>
2023-01-15 02:22:17 +0700

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -42,7 +42,14 @@ const Contact = () => {
           </div>
         </div>
         <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <NetlifyForm honeypotName='bot-field' name="Contact" action="/thanks">
+          <NetlifyForm
+            honeypotName='bot-field'
+            name="Contact"
+            action="/thanks"
+            onSuccess={ (response, context) => {
+              console.log('Successfully sent form data to Netlify Server')
+              context.formRef.current.reset()
+            }}>
           { ({ handleChange, success, error }) => (
             <>
               <Honeypot />
