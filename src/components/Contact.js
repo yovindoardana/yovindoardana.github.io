63c0f54d5e06b7da47286e6fd57f6e6e1add4597
yovindoardana <yovindoardanaa@gmail.com>
2023-01-16 00:05:21 +0700

    Temporary Finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
   const [email, setEmail] = React.useState('')
   const [message, setMessage] = React.useState('')
 
-  function encode (data) {
+  const encode = (data) => {
     return Object.keys(data)
       .map(
         (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
@@ -13,12 +13,12 @@ const Contact = () => {
       .join('&')
   }
 
-  function handleSubmit (e) {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    fetch('/dist', {
+    fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'test', name, email, message })
+      body: encode({ 'form-name': 'contact', name, email, message })
     })
       .then(() => alert('Message sent!'))
       .catch((error) => alert(error))
@@ -33,9 +33,8 @@ const Contact = () => {
             height="100%"
             title="map"
             className="absolute inset-0"
-            frameBorder={0}
-            marginHeight={0}
-            marginWidth={0}
+            border={0}
+            margin={0}
             style={{ filter: 'opacity(0.7)' }}
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d723.7690081357671!2d112.59844892098302!3d-8.001440590184242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1673542881148!5m2!1sen!2sid"
           />
@@ -64,11 +63,9 @@ const Contact = () => {
           </div>
         </div>
         <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <form data-netlify="true" netlify-honeypot='bot-field' name="Contact" method='post' onSubmit={handleSubmit}>
+          <form data-netlify='true' netlify-honeypot='bot-field' name="contact" method='post' onSubmit={handleSubmit}>
             <h2 className="text-white sm:text-4xl text-3xl mb-1 mt-10 sm:mt-0 font-medium title-font">Contact Me</h2>
-            <div hidden>
-              <input name='form-name' value='Contact' readOnly/>
-            </div>
+            <input type="hidden" name="form-name" value="contact" />
             <div className="relative mb-4">
               <label htmlFor="name" className="leading-7 text-sm text-gray-400">
                 Name
